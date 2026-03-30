ee1e1c649814a0be3bb3176bc1ab6f3ec3ffd8da
Daniil Hizatulin <gizatulindaniil770@gmail.com>
2026-03-30 11:24:17 +0300

    initial commit

---
+++ app/(auth)/sign-in.tsx
@@ -0,0 +1,16 @@
+import { Link } from "expo-router";
+import React from "react";
+import { Text, View } from "react-native";
+
+const SignIn = () => {
+  return (
+    <View>
+      <Text>Sign In</Text>
+      <Link href="/(auth)/sign-up" className="mt-4 rounded bg-primary text-white p-4">
+        Create Account
+      </Link>
+    </View>
+  );
+};
+
+export default SignIn;
